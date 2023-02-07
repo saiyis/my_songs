428e3e2b4368d8a67a06048c83d7f6a44710aaa5
saiyis <120037471+saiyis@users.noreply.github.com>
2023-02-07 09:28:19 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+document.querySelector('.song1').addEventListener('click', function () {
+    var agree = new Audio("songs/Can't agree new mix3.mp3")
+    return agree.play()
+})
+
+
+document.querySelector('.song2').addEventListener('click', function () {
+    var dance = new Audio("songs/Dance with me tonight - EM1 - master high.mp3")
+    return dance.play()
+})
+
+
+
